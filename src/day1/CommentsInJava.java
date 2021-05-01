751354597edde63a300da8687d865f9e7b794a52
Beknazar Suranchiyev <beknazarsuranchiyev@Beknazars-MacBook-Pro.local>
2021-05-01 14:09:01 -0400

    day 1 before lunch

--- src/day1/CommentsInJava.java
+++ src/day1/CommentsInJava.java
@@ -2,8 +2,6 @@ package day1;
 
 public class CommentsInJava {
 	public static void main(String[] args) {
-		// LUNCH BREAK TILL 1:00 PM EST
-
 		// this is print statement
 		System.out.println("Comments in Java");
 
