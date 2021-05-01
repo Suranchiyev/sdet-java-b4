0be772823b4949add341bd0cc937ada37eb821ae
Beknazar Suranchiyev <beknazarsuranchiyev@beknazars-mbp.myfiosgateway.com>
2021-05-01 12:30:12 -0400

    init commit

---
+++ src/day1/CommentsInJava.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class CommentsInJava {
+	public static void main(String[] args) {
+		// LUNCH BREAK TILL 1:00 PM EST
+
+		// this is print statement
+		System.out.println("Comments in Java");
+
+		// comments are here
+
+		/*
+		 * comments are here
+		 */
+
+		/**
+		 * comments are here
+		 */
+	}
+
+}
---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World"); 
+		System.out.println("apple");
+	}	
+}
