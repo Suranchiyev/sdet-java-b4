0c156b8d4ede0cd3607a11eb0de59d4317921a59
Suranchiyev <beknazarsuranchiyev@gmail.com>
2021-05-02 14:57:45 -0400

    day2 after lunch

---
+++ src/day2/Subtraction.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Subtraction {
+	public static void main(String[] args) {
+		int num1 = 23;
+		int num2 = 13;
+		
+		int result = num1 - num2;
+		System.out.println(result);
+	}
+}
